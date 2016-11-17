02315ecbf0ba4ad3c21add8c30ab042a0a740afb
jixing475 <jixing475@gmail.com>
2016-11-17 13:00:30 +0800

    获得 unique sample ID

---
+++ ZERO/20161117_get_ID.sh
@@ -0,0 +1,14 @@
+ls  tophat_results/*/accept_hits.bam | while read id ; 
+sampleID=` echo $id | cut -d"/" -f 2`
+do 
+nohup 
+~/biosoft/Cufflinks/current/cufflinks -p 20 -o  cufflinks_results/$sampleID  $id 
+done 
+
+
+[jixing@log01 liuyouhua_20161029]$ 
+ls alignment/*.bam | while read id; 
+sampleID=` echo $id | cut -d "/" -f 2 | cut -d "." -f 1`; 
+do 
+echo $sampleID;
+done > ID.txt
